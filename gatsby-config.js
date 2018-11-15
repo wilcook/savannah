d903b10db3997a07a8a118b046cb8d2627e3d6e0
wilcook <wil.cook.is@gmail.com>
2018-11-15 14:38:05 -0500

    fix site color

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#855a82',
+        theme_color: '#855a82',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
