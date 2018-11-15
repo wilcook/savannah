41ab7b9c5c9d8d94d16506dcd35daaf6e13dd51e
wilcook <wil.cook.is@gmail.com>
2018-11-15 14:26:36 -0500

    spacing fix on header attempt #2

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,8 +10,9 @@ class Header extends React.Component {
                 <div className="inner">
                     <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
                     <h1><strong>Savannah Schmidt MUA</strong><br />
-                    <br />
+                    <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed turpis mattis, eleifend nibh a, finibus massa. Nunc elementum semper felis non imperdiet. Praesent mauris lorem, feugiat nec commodo non, vestibulum ac ipsum. Morbi in augue cursus, molestie lectus a, pulvinar ipsum.
+                  </p>
                     </h1>
                 </div>
                 <Footer />
